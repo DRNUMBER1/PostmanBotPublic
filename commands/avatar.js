70fdb507f22b72c289b8d0f82a7751c36f13684a
Rykun <66620681+DRNUMBER1@users.noreply.github.com>
2021-04-09 14:38:51 +0530

    avatar cmd

---
+++ commands/avatar.js
@@ -0,0 +1,19 @@
+const discord = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    description: "Shows someone's avatar!",
+    execute(msg, args) {
+
+        const useravatar = msg.mentions.users.first()
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Showing avatar for ${useravatar}`)
+        .setAuthor(msg.author)
+        .setImage(useravatar.avatarURL)
+        msg.channel.send(embed)
+
+
+
+
+    }
+}
