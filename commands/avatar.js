b05bb354f947c8a8be89718cbde9d943c65cdef2
Rykun <66620681+DRNUMBER1@users.noreply.github.com>
2021-04-09 14:41:27 +0530

    avatar fix

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Shows someone's avatar!",
     execute(msg, args) {
 
-        const useravatar = msg.mentions.users.first()
+        const useravatar =  msg.mentions.users.first();
         const embed = new Discord.MessageEmbed()
         .setTitle(`Showing avatar for ${useravatar}`)
         .setAuthor(msg.author)
