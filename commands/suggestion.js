7ea4bcf28496d5a82cd2accd83fb9e68ec124e75
Rykun <66620681+DRNUMBER1@users.noreply.github.com>
2021-04-10 09:31:28 +0530

    added suggestion cmd

---
+++ commands/suggestion.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'suggestion',
+    description: "Reacts at a suggestion",
+    execute(client, msg, args) {
+
+        let suggest = agrs[0];
+
+        if (!suggest) {
+            msg.channel.send("please provide the suggestion!").then(async (e) => {
+                await e.delete({
+                    timeout: 5000
+                })
+            })
+            return;
+
+        } else {
+            msg.react('👍');
+            msg.react('👎');
+        }
+    }
+}
